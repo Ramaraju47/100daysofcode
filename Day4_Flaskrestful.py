13ac2421f9605280ae46b63234329b31d3fffe8e
Ramaraju47 <35668071+Ramaraju47@users.noreply.github.com>
2019-10-07 10:59:18 -0400

    Add files via upload

---
+++ Day4_Flaskrestful.py
@@ -0,0 +1,26 @@
+from flask import Flask,request
+from flask_restful import Resource,Api
+
+app=Flask(__name__)
+api=Api(app)
+
+items=[]
+
+class Item(Resource):
+    def get(self,name):
+        for i in items:
+            if i['name']==name:
+                return i
+        return {'item':None},404
+    def post(self,name):
+        data=request.get_json()
+        item={'name': name,'price':data['price']}
+        items.append(item)
+        return item,201
+
+class Itemlist(Resource):
+    def get(self):
+        return {'items':items}
+
+api.add_resource(Item,'/item/<string:name>')
+app.run(port=5000,debug=True)
